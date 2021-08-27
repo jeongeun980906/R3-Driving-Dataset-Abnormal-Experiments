a90124df39cab7aa22e51025780712be698a310c
JeongEun <baro0906@korea.ac.kr>
2021-08-27 17:06:40 +0900

    minor bug

--- script/mdn.sh
+++ script/mdn.sh
@@ -1,8 +1,8 @@
 cd ..
-python main.py --gpu 1 --init_pool 500 --query_size 250 --exp_case 2 3 --id 8 --n_object 1
-python main.py --gpu 1 --query_method aleatoric --init_pool 500 --query_size 250 --exp_case 2 3 --id 8 --n_object 1
-python main.py --gpu 1 --query_method pi_entropy --init_pool 500 --query_size 250 --exp_case 2 3 --id 8 --n_object 1
-python main.py --gpu 1 --query_method random --init_pool 500 --query_size 250 --exp_case 2 3 --id 8 --n_object 1 
+python main.py --gpu 1 --init_pool 100 --query_size 300 --exp_case 2 3 --id 11 --n_object 1
+python main.py --gpu 1 --query_method aleatoric --init_pool 100 --query_size 300 --exp_case 2 3 --id 11 --n_object 1
+python main.py --gpu 1 --query_method pi_entropy --init_pool 100 --query_size 300 --exp_case 2 3 --id 11 --n_object 1
+python main.py --gpu 1 --query_method random --init_pool 100 --query_size 300 --exp_case 2 3 --id 11 --n_object 1 
 # python main.py --gpu 0 --exp_case 1 --init_pool 100 --query_size 50
 # python main.py --query_method aleatoric --gpu 0 --exp_case 1 --init_pool 100 --query_size 50
 # python main.py --query_method pi_entropy --gpu 0 --exp_case 1 --init_pool 100 --query_size 50
@@ -13,7 +13,7 @@ python main.py --gpu 1 --query_method random --init_pool 500 --query_size 250 --
 # python main.py --query_method pi_entropy --gpu 0 --exp_case 1 --init_pool 100 --query_size 50
 # python main.py --query_method random --gpu 0 --exp_case 1 --init_pool 100 --query_size 50
 
-# python main.py --gpu 1 --init_pool 500 --query_size 250 
-# python main.py --query_method aleatoric --gpu 1 --init_pool 500 --query_size 250
-# python main.py --query_method pi_entropy --gpu 1 --init_pool 500 --query_size 250
-# python main.py --query_method random --gpu 1 --init_pool 500 --query_size 250
+# python main.py --gpu 1 --init_pool 100 --query_size 200 
+# python main.py --10uery_method aleatoric --gpu 1 --init_pool 100 --query_size 100
+# python main.py --q10ery_method pi_entropy --gpu 1 --init_pool 100 --query_size 100
+# python main.py --q10ery_method random --gpu 1 --init_10ool 100 --query_size 510
